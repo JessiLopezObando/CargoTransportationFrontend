00fc242255d726bd9294df9d1dea90b82cfdbb64
zensei <diegofer_1110@hotmail.com>
2023-04-24 15:49:18 -0500

    Login page and login component

--- src/app/components/forms/login/login.component.ts
+++ src/app/components/forms/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +13,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  myForm: FormGroup = new FormGroup({});
+
+  constructor(
+    private fb: FormBuilder,
+  ) {}
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+    });
+  }
+
+  async onSubmit(form: FormGroup) {
+    try {
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  get email() {
+    return this.myForm.get('email');
+  }
+
+  get password() {
+    return this.myForm.get('password');
+  }
 
 }
--- src/app/pages/login-page/login-page.component.spec.ts
+++ src/app/pages/login-page/login-page.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { LoginPageComponent } from './login-page.component';
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+describe('LoginPageComponent', () => {
+  let component: LoginPageComponent;
+  let fixture: ComponentFixture<LoginPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginPageComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(LoginPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
