0daa28c00a6fa8d1e7a4072653607527c2520da3
zensei <diegofer_1110@hotmail.com>
2023-04-24 15:50:41 -0500

    not found page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:"",
+    pathMatch:"full",
+    redirectTo:"home"
+  },
+  {
+    path:"home",
+    component: HomeComponent
+  },
+  {
+    path:"login",
+    component: LoginPageComponent
+  },
+  { path: '**', pathMatch: 'full', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,21 @@ import { RegisterComponent } from './components/forms/register/register.componen
 import { LoginComponent } from './components/forms/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatStepperModule} from '@angular/material/stepper';
+import { DriverComponent } from './components/forms/driver/driver.component';
+import { VehicleComponent } from './components/forms/vehicle/vehicle.component';
+
 
 @NgModule({
   declarations: [
@@ -16,11 +31,25 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
     RegisterComponent,
     LoginComponent,
     HomeComponent,
-    DashboardComponent
+    DashboardComponent,
+    LoginPageComponent,
+    NotFoundComponent,
+    DriverComponent,
+    VehicleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatIconModule,
+    MatStepperModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
