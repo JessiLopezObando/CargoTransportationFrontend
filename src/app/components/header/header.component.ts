bae1235fad0f1bde458be52b55c434366c6fb755
zensei <diegofer_1110@hotmail.com>
2023-04-25 15:02:12 -0500

    header style and logic done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Driver } from 'src/app/models/driver';
+import { DriverService } from 'src/app/services/driverService/driver.service';
+import { TokenService } from 'src/app/services/tokenService/token.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +11,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+  isLogged: boolean = false;
+  driver : Driver | undefined;
+
+  constructor(
+    private driverService: DriverService,
+    private tokenService: TokenService,
+    private router: Router,
+    private jwtAuth:JwtHelperService
+    ) {}
+
+
+  ngOnInit(): void {
+    this.updateDriverHeader();
+    this.checkIfLogged();
+  }
+
+
+  updateDriverHeader(){
+    this.tokenService.loadToken();
+    this.tokenService.getToken().subscribe((token) => {
+      if (token) {
+        this.driverService.getDriverByEmail(this.jwtAuth.decodeToken(token).email).subscribe((customer) => {
+          this.driver = customer;
+        } );
+      }
+    } );
+  }
+
+  checkIfLogged() {
+    this.tokenService.isLogged().subscribe((logged) => {
+      this.isLogged = logged;
+      });
+  }
+
+  logout() {
+    this.tokenService.closeSession();
+    this.updateDriverHeader();
+    this.router.navigate([this.router.url], { skipLocationChange: true }).then(() => {
+      window.location.reload();
+    });
+  }
+
+  isDashboardRoute(): boolean {
+    const currentUrl = this.router.url;
+    return currentUrl === '/dashboard';
+  }
+  
 
 }
--- src/app/guards/auth-fire.guard.ts
+++ src/app/guards/auth-fire.guard.ts
@@ -15,14 +15,18 @@ export class AuthFireGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-      let token = '';
+      let isLogged:boolean = true;
 
       this.tokenService.loadToken();
 
-      this.tokenService.getToken().subscribe(tk=>token=tk)
+      //this.tokenService.getToken().subscribe(tk=>token=tk)
+
+      this.tokenService.isLogged().subscribe(logged=>{
+        isLogged=logged;
+      })
   
-      if (token) {
-        if (!this.authJwt.isTokenExpired(token)) return true;
+      if (isLogged) {
+        return true;
       }
   
       this.router.navigate(['/login']);
