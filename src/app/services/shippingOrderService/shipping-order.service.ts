c2529d93556d3c3611826ed30238cd250440ec8d
zensei <diegofer_1110@hotmail.com>
2023-04-26 15:45:50 -0500

    custom methods for driver and shipping order added

--- src/app/services/shippingOrderService/shipping-order.service.ts
+++ src/app/services/shippingOrderService/shipping-order.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { urlBackend } from 'src/environments/settingsToStart';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShippingOrderService {
 
-  constructor() { }
+  path:string = 'tickets/'
+
+  constructor(private http: HttpClient) { }
+
+  getShippingOrdersForDriverByStatus(driverId:string,status:string){
+    return this.http.get(urlBackend+this.path+'driverId/'+driverId+'/status/'+status);
+  }
+
+  acceptedShippingOrder(ticketId:string){
+    return this.http.patch(urlBackend+this.path+ticketId+'/accepted',{});
+  }
+
+  refuseShippingOrder(ticketId:string){
+    return this.http.patch(urlBackend+this.path+ticketId+'/refused',{});
+  }
+
+  deliveredShippingOrder(ticketId:string){
+    return this.http.patch(urlBackend+this.path+ticketId+'/delivered',{});
+  }
+
+  quoteShippingOrder(minutes:number,weight:number){
+    return this.http.get(urlBackend+this.path+'cost/minutes/'+minutes+'/weight/'+weight);
+  }
+
 }
