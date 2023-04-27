1c7589910b9297baf259f40afa14caceca814713
William Serna <williamsernac@hotmail.com>
2023-04-27 14:22:46 -0500

    Implemented disabling of "Accept" Button in Dashboard
    Implemented disabling of "Accept" in incoming and available tickets when vehicle's weight capacity is reached

--- src/app/components/cards/order-information/order-information.component.ts
+++ src/app/components/cards/order-information/order-information.component.ts
@@ -1,19 +1,36 @@
+import { OnInit } from '@angular/core';
+import { OnChanges } from '@angular/core';
+import { SimpleChanges } from '@angular/core';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Driver } from 'src/app/models/driver';
 import { ShippingOrder } from 'src/app/models/shippingOrder';
+import { Vehicle } from 'src/app/models/vehicle';
 
 @Component({
   selector: 'app-order-information',
   templateUrl: './order-information.component.html',
   styleUrls: ['./order-information.component.scss']
 })
-export class OrderInformationComponent {
+export class OrderInformationComponent implements OnInit, OnChanges{
 
   @Input() shippingOrder:ShippingOrder | undefined;
+  @Input() driver:Driver | undefined;  
   @Output() shippingOrderAccepted:EventEmitter<string> = new EventEmitter<string>();
   @Output() shippingOrderRejected:EventEmitter<string> = new EventEmitter<string>();
   @Output() shippingOrderDelivered:EventEmitter<string> = new EventEmitter<string>();
 
-  constructor() { }
+  isDisabled: boolean = false;
+  weight: number = 0;  
+
+  ngOnInit(){
+    this.weight = (this.driver?.vehicle?.currentCapacity || 0) + (this.shippingOrder?.weight || 0)    
+    this.availableWeight();
+  }
+  ngOnChanges(changes: SimpleChanges) {
+
+    this.availableWeight();
+
+  }
 
   onAccept(){
     this.shippingOrderAccepted.emit(this.shippingOrder?.id);
@@ -27,5 +44,32 @@ export class OrderInformationComponent {
     this.shippingOrderDelivered.emit(this.shippingOrder?.id);
   }
 
+  private availableWeight(){
+    this.weight = (this.driver?.vehicle?.currentCapacity || 0) + (this.shippingOrder?.weight || 0)
+    
+
+    if (this.weight > (this.driver?.vehicle?.totalCapacity || 0)){
+      
+      this.isDisabled = true
+    }
+    else {
+      this.isDisabled = false
+    }    
+
+  }
 
 }
+  
+  
+
+  
+
+
+  
+    
+    
+
+  
+
+
+
